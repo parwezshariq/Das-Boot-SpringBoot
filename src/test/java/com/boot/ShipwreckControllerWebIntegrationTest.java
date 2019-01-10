380f73602aa1bc7a25e659d23708ce3f54264e34
shariqparwez <shariqparwez@outlook.com>
2019-01-10 19:13:13 +0530

    Spring Boot - Unit Testing and Integration Testing

---
+++ src/test/java/com/boot/ShipwreckControllerWebIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ShipwreckControllerWebIntegrationTest {
+
+	@Test
+	public void testListAll() throws IOException {
+		TestRestTemplate restTemplate = new TestRestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/shipwrecks", String.class);
+		
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode responseJson = objectMapper.readTree(response.getBody());
+		
+		assertThat(responseJson.isMissingNode(), is(false));
+		assertThat(responseJson.toString(), equalTo("[]"));
+	}
+	
+}
